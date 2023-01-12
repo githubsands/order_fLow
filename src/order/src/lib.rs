0f476929b011ac92b8db4ad7818a049a522cccb1
Ryan Vacek <ryanvacek@Ryans-MBP.lan>
2023-01-12 11:16:57 -0700

    /design some order additions

--- src/order/src/lib.rs
+++ src/order/src/lib.rs
@@ -1,66 +1,77 @@
+use std::boxed::Box;
+use std::error::Error;
+use std::iter::Iterator;
 use std::vec;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Order {
-    id: u64,
+    id: u8,
 }
 
 pub enum OrdersManagerError {
-    OrdersNonExist,
-    OrderDoesntExist(u64),
+    OrdersNonExistent,
+    OrderDoesntExist(u8),
 }
 
 #[derive(Clone, Debug)]
 pub struct OrderManager {
     orders: Vec<Order>,
+    count: usize,
 }
 
 impl OrderManager {
     fn new() -> Self {
-        Self { orders: vec![] }
+        Self {
+            orders: vec![],
+            count: 0,
+        }
     }
+
     fn add_order(&mut self, order: Order) {
         self.orders.push(order)
     }
 
     fn orders(self) -> Result<Vec<Order>, OrdersManagerError> {
         if self.orders.len() == 0 {
-            Err(OrdersManagerError::OrdersNonExist)
+            Err(OrdersManagerError::OrdersNonExistent)?
         }
-        Ok(self.orders);
+        Ok(self.orders)
     }
 
     fn orders_add(&mut self, mut orders: Vec<Order>) {
         self.orders.append(&mut orders)
     }
 
-    fn get_order(&self, id: u64) -> Result<Order, OrdersManagerError> {
+    fn get_order(&self, id: u8) -> Result<Order, OrdersManagerError> {
         for (_index, value) in self.orders.iter().enumerate() {
             if value.id == id {
                 let order = value.clone();
-                Ok(order);
+                return Ok(order);
             }
         }
-        Err(OrdersManagerError::OrderDoesntExist(id));
+        Err(OrdersManagerError::OrderDoesntExist(id))?
     }
 
-    fn remove_order(&mut self, id: u64) -> Result<u8, OrdersManagerError> {
+    fn remove_order(&mut self, id: u8) -> Result<u8, OrdersManagerError> {
         for order in &mut self.orders {
             if order.id == id {
                 order.id = 0;
-                Ok(1);
+                return Ok(id);
             }
         }
-        Err(OrdersManagerError::OrderDoesntExist(id));
+        Err(OrdersManagerError::OrderDoesntExist(id))?
     }
 }
 
-/*
-trait OrderHandles {
-    fn add_order(&self, order: Order);
-    fn orders_add(&self, orders: Order);
-    fn orders(self, order: Order) -> Result<Vec<Order>, OrdersManagerError>;
-    fn get_order(&self, id: u64);
-    fn remove_order(&self, id: u64) -> Result<Vec<Order>, OrdersManagerError>;
+impl Iterator for OrderManager {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count < self.orders.len() {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
 }
-*/
